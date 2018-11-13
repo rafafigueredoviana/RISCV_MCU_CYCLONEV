776b5573b52dd87f3202a62c1bd57eff8b1ba985
Rafael de Figueredo Viana <rafaeldefigueredoviana@outlook.com>
2018-11-12 22:43:15 -0300

    Firmware test

--- Project/Firmware/Eclipse/RISCV_MCU_FPGA/src/main.c
+++ Project/Firmware/Eclipse/RISCV_MCU_FPGA/src/main.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
-#define DATA_RAM_BASE 0x100000;
+#define DATA_RAM_BASE 	0x00100000
+#define APB_BASE_ADDR 	0x1A100000
+
+#define UART_APB_BASE   0x00000000
+#define GPIO_APB_BASE	0x00001000
+
+#define GPIO_ADDR (APB_BASE_ADDR + GPIO_APB_BASE)
 
 int main()
 {
 
-  int *pointer;
+
+  int *pointer_ram;
+  int *pointer_apb;
   int value = 10;
-  pointer = (int *) DATA_RAM_BASE;
+  pointer_ram = (int *) DATA_RAM_BASE;
+  pointer_apb = (int *) GPIO_ADDR;
+  pointer_apb++;
+  pointer_ram++;
 
   for (int i = 0; i < 10; i++){
-	  *pointer = value;
-	  pointer++;
+	  *pointer_ram = value;
+	  pointer_ram++;
 	  value++;
 
   }
 
-
+  *pointer_apb = 1;
 
   return 0;
 }
